30267e2cda7661b923a3ea0571662e36a80a09af
leesh <qwqw8019@naver.com>
2023-03-29 19:30:10 +0900

    feat: 마이페이지 매핑 생성

--- src/main/java/hicc/toy_project/controller/myPageApiController.java
+++ src/main/java/hicc/toy_project/controller/myPageApiController.java
@@ -3,17 +3,41 @@ package hicc.toy_project.controller;
 import hicc.toy_project.domain.member.Member;
 import hicc.toy_project.domain.member.Role;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class myPageApiController {
-    @GetMapping("/mypage/info")
+    @GetMapping("/mypage/{id}")
     @ResponseBody
-    public Member myPageInfo(@RequestParam("id") long id){
-        Member member = new Member(id, Role.PRESIDENT);
+    public Member myPageInfo(@PathVariable long id){
+        Member member = new Member(id, Role.PRESIDENT,"aaa");
         return member;
+    }
+
+    @PatchMapping("/mypage/{id}/modify")
+    @ResponseBody
+    public void myPageModify(@PathVariable long id){
+
+    }
+
+    @GetMapping("/mypage/{id}/post")
+    @ResponseBody
+    public void myPagePost(){
+
+    }
+
+    @GetMapping("/mypage/{id}/comment")
+    @ResponseBody
+    public void myPageComment(){
+
+    }
+
+
+    @GetMapping("/mypage/{id}/rental")
+    @ResponseBody
+    public String myPageRental(){
+
+        return "rentals";
     }
 
 
