b00af7ce0f9e04fcf3aece9b477217c65b3d31c2
leesh <qwqw8019@naver.com>
2023-03-29 17:56:59 +0900

    feat: spring 프로젝트 생성

---
+++ src/main/java/hicc/toy_project/ToyProjectApplication.java
@@ -0,0 +1,13 @@
+package hicc.toy_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hicc/toy_project/ToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hicc.toy_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
