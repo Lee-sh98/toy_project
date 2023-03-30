9c9f5a97d5a859dc9c206d6a7ebc05b30445c974
leesh <qwqw8019@naver.com>
2023-03-30 20:25:20 +0900

    feat: Member 레포지토리 생성

---
+++ src/main/java/hicc/toy_project/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package hicc.toy_project.repository;
+
+import hicc.toy_project.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+}
