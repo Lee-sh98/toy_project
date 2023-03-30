3bd82928afbb050c3707217450565a37e7a351ad
leesh <qwqw8019@naver.com>
2023-03-30 20:26:47 +0900

    feat: 전화번호, 전공 필드 추가

--- src/main/java/hicc/toy_project/domain/member/Member.java
+++ src/main/java/hicc/toy_project/domain/member/Member.java
@@ -1,15 +1,28 @@
 package hicc.toy_project.domain.member;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
+@Entity
 @Getter
+@Setter
+@RequiredArgsConstructor
 public class Member {
 
-    private long id;
+    @Id
+    private final String id;
     private Role role;
+    private String nickName;
+    private String phoneNumber;
+    private String major;
 
-    public Member(long id, Role role){
+
+    public Member(String id, Role role, String nickName) {
         this.id = id;
         this.role = role;
+        this.nickName = nickName;
     }
 }
