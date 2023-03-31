5849b25059949e9ce14f22df2431af7be3c7d55b
leesh <qwqw8019@naver.com>
2023-03-31 15:13:03 +0900

    feat: 멤버 아이디에 의한 게시글리스트 검색 메소드 구현

--- src/main/java/hicc/toy_project/repository/PostRepository.java
+++ src/main/java/hicc/toy_project/repository/PostRepository.java
@@ -4,6 +4,9 @@ import hicc.toy_project.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post,String> {
+    List<Post> findAllByMemberId(String id);
 }
