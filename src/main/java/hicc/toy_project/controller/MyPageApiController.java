cce17051a63767eb022b1e3badff98334d9af06c
leesh <qwqw8019@naver.com>
2023-04-09 21:38:58 +0900

    comment: 마이페이지 컨트롤러 주석 작성

--- src/main/java/hicc/toy_project/controller/MyPageApiController.java
+++ src/main/java/hicc/toy_project/controller/MyPageApiController.java
@@ -16,24 +16,52 @@ import java.util.List;
 public class MyPageApiController {
     private final MyPageService myPageService;
 
+    /**
+     * 나의 정보를 확인한다.
+     * @param request Body 부분에 Member Id를 넣어야 한다.
+     *                Ex) {"id":"X011001"}
+     * @return Member
+     */
     @GetMapping("/mypage/info")
     @ResponseBody
     public Member myPageInfo(@RequestBody MyPageRequest request) {
         return myPageService.memberInfo(request.getId());
     }
 
+    /**
+     * 나의 정보를 수정한다.
+     * @param request Body 부분에 Member Id를 비롯한 수정할 사항을 입력한다.
+     *                Ex) {
+     *                "id": "X011001",
+     *                "nickName": "modifiedNickName",
+     *                "phoneNumber": "01012341234"
+     *                }
+     * @return 정보 수정이 완료되면 True, 아닐 경우 False
+     */
     @PostMapping("/mypage/modify")
     @ResponseBody
     public Boolean myPageModify(@RequestBody MyPageRequest request) {
         return myPageService.memberModify(request);
     }
 
+    /**
+     * 내가 작성한 글을 확인한다.
+     * @param request Body 부분에 Member Id를 넣어야 한다.
+     *                Ex) {"id":"X011001"}
+     * @return List of PostResponse DTO
+     */
     @GetMapping("/mypage/post")
     @ResponseBody
     public List<PostResponse> myPagePost(@RequestBody MyPageRequest request) {
         return myPageService.myPost(request.getId());
     }
 
+    /**
+     * 내가 작성한 댓글을 확인한다.
+     * @param request Body 부분에 Member Id를 넣어야한다.
+     *                Ex) {"id":"X011001"}
+     * @return List of CommentResponse DTO
+     */
     @GetMapping("/mypage/comment")
     @ResponseBody
     public List<CommentResponse> myPageComment(@RequestBody MyPageRequest request) {
