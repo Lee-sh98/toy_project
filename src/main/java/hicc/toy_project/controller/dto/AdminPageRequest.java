afbcb1b8b8d569112cfc05d454a9e9c3795604c3
leesh <qwqw8019@naver.com>
2023-04-10 21:21:52 +0900

    feat: AdminPageRequest DTO에 targetId필드 추가

--- src/main/java/hicc/toy_project/controller/dto/AdminPageRequest.java
+++ src/main/java/hicc/toy_project/controller/dto/AdminPageRequest.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AdminPageRequest {
     private String id;
+    private String targetId;
     private Role role;
     private String nickname;
     private String phoneNumber;
