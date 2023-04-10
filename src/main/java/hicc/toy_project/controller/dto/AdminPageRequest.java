42d9f3430a348a8e7f69d8fdfb63596d5da73210
leesh <qwqw8019@naver.com>
2023-04-10 19:41:48 +0900

    feat: 어드민 페이지 리퀘스트 DTO 구현

---
+++ src/main/java/hicc/toy_project/controller/dto/AdminPageRequest.java
@@ -0,0 +1,23 @@
+package hicc.toy_project.controller.dto;
+
+import hicc.toy_project.domain.member.Member;
+import hicc.toy_project.domain.member.Role;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AdminPageRequest {
+    private String id;
+    private Role role;
+    private String nickname;
+    private String phoneNumber;
+
+    @Builder
+    public Member toEntity(){
+        return Member.builder()
+                .id(id)
+                .build();
+    }
+}
