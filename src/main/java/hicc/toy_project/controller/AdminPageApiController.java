1a79d99092c1031a6434681b52ddd36ac0f37f60
leesh <qwqw8019@naver.com>
2023-04-11 15:06:53 +0900

    feat: 회원 정보 수정 라우팅

--- src/main/java/hicc/toy_project/controller/AdminPageApiController.java
+++ src/main/java/hicc/toy_project/controller/AdminPageApiController.java
@@ -3,9 +3,11 @@ package hicc.toy_project.controller;
 import hicc.toy_project.controller.dto.AdminPageRequest;
 import hicc.toy_project.controller.dto.MemberResponse;
 import hicc.toy_project.service.AdminPageService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,14 +20,19 @@ public class AdminPageApiController {
 
     /**
      * Body 부분에 들어간 id 값을 가진 멤버가 회장일 경우 모든 회원의 리스트를 반환한다.
+     *
      * @param request {"id":[회장의 id]}
      * @return List of Members
      */
     @GetMapping("/admin/members")
     @ResponseBody
-    public List<MemberResponse> members(@RequestBody AdminPageRequest request){
+    public List<MemberResponse> members(@RequestBody AdminPageRequest request) {
         return adminPageService.members(request.getId());
     }
 
-
+    @PostMapping("/admin/modify")
+    @ResponseBody
+    public boolean memberModify(@RequestBody AdminPageRequest request) {
+        return adminPageService.memberModify(request);
+    }
 }
