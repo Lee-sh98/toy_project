ec014a893dabe92dedfc6165eee6a715a66b84aa
leesh <qwqw8019@naver.com>
2023-04-11 15:03:03 +0900

    feat: 회원 정보 수정 기능 구현 및 회장 확인 로직 리팩토링

--- src/main/java/hicc/toy_project/service/AdminPageService.java
+++ src/main/java/hicc/toy_project/service/AdminPageService.java
@@ -1,5 +1,6 @@
 package hicc.toy_project.service;
 
+import hicc.toy_project.controller.dto.AdminPageRequest;
 import hicc.toy_project.controller.dto.MemberResponse;
 import hicc.toy_project.domain.member.Role;
 import hicc.toy_project.repository.MemberRepository;
@@ -16,15 +17,30 @@ import java.util.stream.Collectors;
 public class AdminPageService {
     private final MemberRepository memberRepository;
 
+    private boolean isPresident(String id) {
+        return memberRepository.findById(id).isPresent()
+                && memberRepository.findById(id).get().getRole().equals(Role.PRESIDENT);
+    }
+
     @Transactional
-    public List<MemberResponse> members(String id){
-        if (memberRepository.findById(id).isPresent()
-                && memberRepository.findById(id).get().getRole().equals(Role.PRESIDENT)) {
+    public List<MemberResponse> members(String id) {
+        if (isPresident(id)) {
             return memberRepository.findAll()
                     .stream()
                     .map(MemberResponse::new)
                     .collect(Collectors.toList());
         }
         return new ArrayList<>();
+    }
+
+    @Transactional
+    public boolean memberModify(AdminPageRequest request) {
+        if (isPresident(request.getId()) && !request.getId().equals(request.getTargetId())) {
+            return memberRepository.findById(request.getTargetId())
+                    .map(member ->
+                            member.update(request.getNickName(), request.getPhoneNumber()))
+                    .orElse(false);
+        }
+        return false;
     }
 }
