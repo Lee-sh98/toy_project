cce0807e28562403e28da75daaadc2bb34483c53
leesh <qwqw8019@naver.com>
2023-06-28 14:41:18 +0900

    feat #2 : PostType 의 본문 길이 제한
    for limit length of content

--- src/main/java/hicc/toy_project/domain/post/PostType.java
+++ src/main/java/hicc/toy_project/domain/post/PostType.java
@@ -1,11 +1,18 @@
 package hicc.toy_project.domain.post;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum PostType {
-    NOTICE,
-    FREE,
-    ACTIVITY,
-    PREVIOUS_TEST,
-    EMPLOYMENT,
-    GRADUATE,
-    MUST_EAT
+    NOTICE(1000),
+    FREE(1000),
+    ACTIVITY(1000),
+    PREVIOUS_TEST(1000),
+    EMPLOYMENT(1000),
+    GRADUATE(1000),
+    MUST_EAT(100);
+
+    private final int contentLength;
 }
