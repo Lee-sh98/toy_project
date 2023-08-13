b34e6fbfcd92800d86ad18fc66475869e6605ec7
leesh <qwqw8019@naver.com>
2023-08-13 16:54:35 +0900

    feat: ApproveResponse 생성 (#9)

---
+++ src/main/java/hicc/toy_project/controller/dto/ApproveResponse.java
@@ -0,0 +1,8 @@
+package hicc.toy_project.controller.dto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ApproveResponse {
+    private final ApproveStatus status;
+}
