68a13cdb7ae858b3b0801f8b6d206cb72dec2f70
leesh <qwqw8019@naver.com>
2023-09-22 05:35:12 +0900

    feat: 대여 요청 dto 구현 (#5)

---
+++ src/main/java/hicc/toy_project/controller/dto/request/rental/RentalRequest.java
@@ -0,0 +1,15 @@
+package hicc.toy_project.controller.dto.request.rental;
+
+
+import hicc.toy_project.domain.rental.RentalStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RentalRequest {
+
+    private String targetId;
+    private RentalStatus status;
+
+}
