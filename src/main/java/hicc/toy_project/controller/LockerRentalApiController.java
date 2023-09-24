437688bdcf1386e2c12999cfe11ce8ba98b47cfe
leesh <qwqw8019@naver.com>
2023-09-24 21:18:08 +0900

    feat: 사물함 대여 컨트롤러 구현 (#5)

---
+++ src/main/java/hicc/toy_project/controller/LockerRentalApiController.java
@@ -0,0 +1,66 @@
+package hicc.toy_project.controller;
+
+
+import hicc.toy_project.controller.dto.request.rental.LockerRentalRequest;
+import hicc.toy_project.controller.dto.response.rental.LockerListResponse;
+import hicc.toy_project.controller.dto.response.rental.LockerSimpleResponse;
+import hicc.toy_project.exception.CustomException;
+import hicc.toy_project.exception.ErrorCode;
+import hicc.toy_project.service.LockerRentalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin/locker")
+@RequiredArgsConstructor
+public class LockerRentalApiController {
+
+    private final LockerRentalService lockerRentalService;
+
+    /*
+    사물함 대여하기 기능
+    개발을 위해 구현하였고 추후 제거 필요
+     */
+    public LockerSimpleResponse rentLocker(@RequestBody LockerRentalRequest request) {
+        lockerRentalService.validateEligibleMember(request.getLessorId());
+
+        return lockerRentalService.rentLocker(request);
+    }
+
+    // 사물함 대여 현황 조회
+    @GetMapping
+    public LockerListResponse listLocker() {
+        String presidentId = "C011001";
+        lockerRentalService.validatePresident(presidentId);
+
+        return lockerRentalService.listLocker();
+    }
+
+    // 사물함 대여 승인/거부
+    @PatchMapping
+    public LockerSimpleResponse processRental(@RequestParam("status") String status, @RequestBody LockerRentalRequest request) {
+        String presidentId = "C011001";
+        lockerRentalService.validatePresident(presidentId);
+
+        if (status.equals("approve")) {
+            return lockerRentalService.approveRental(request);
+        }
+        if (status.equals("reject")) {
+            return lockerRentalService.rejectRental(request);
+        }
+
+        throw new CustomException(ErrorCode.INVALID_REQUEST);
+    }
+
+
+    // 사물함 관리
+    @PatchMapping("/manage")
+    public LockerSimpleResponse manageLocker(@RequestBody LockerRentalRequest request) {
+        String presidentId = "C011001";
+        lockerRentalService.validatePresident(presidentId);
+
+        return lockerRentalService.manageLocker(request);
+    }
+
+}
