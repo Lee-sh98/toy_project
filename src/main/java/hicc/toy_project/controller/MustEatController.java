6c8d9ee253d0ea17210ed3a47cfbe0d64815c0bc
leesh <qwqw8019@naver.com>
2023-09-28 02:06:23 +0900

    rename: 맛집 등록 응답 dto 파일명 변경 (#17)

--- src/main/java/hicc/toy_project/controller/MustEatController.java
+++ src/main/java/hicc/toy_project/controller/MustEatController.java
@@ -1,10 +1,9 @@
 package hicc.toy_project.controller;
 
 import hicc.toy_project.controller.dto.request.mustEat.MustEatCreateRequest;
-import hicc.toy_project.controller.dto.response.mustEat.MustEatSimpleResponse;
+import hicc.toy_project.controller.dto.response.mustEat.MustEatResponse;
 import hicc.toy_project.service.MustEatService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +14,7 @@ public class MustEatController {
     private final MustEatService mustEatService;
 
     @PostMapping
-    public MustEatSimpleResponse create(@RequestBody MustEatCreateRequest request) {
+    public MustEatResponse create(@RequestBody MustEatCreateRequest request) {
 
         return mustEatService.create(request);
     }
