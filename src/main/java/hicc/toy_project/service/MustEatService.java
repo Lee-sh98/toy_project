8ba711b88e463d20f86c9332eb852a84ca09f082
leesh <qwqw8019@naver.com>
2023-09-28 04:07:03 +0900

    feat: 맛집 조회 기능 구현 (#17)

--- src/main/java/hicc/toy_project/service/MustEatService.java
+++ src/main/java/hicc/toy_project/service/MustEatService.java
@@ -2,14 +2,19 @@ package hicc.toy_project.service;
 
 
 import hicc.toy_project.controller.dto.request.mustEat.MustEatCreateRequest;
+import hicc.toy_project.controller.dto.response.mustEat.MustEatListResponse;
 import hicc.toy_project.controller.dto.response.mustEat.MustEatResponse;
 import hicc.toy_project.domain.mustEat.MustEat;
 import hicc.toy_project.repository.MustEatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MustEatService {
 
     private final MustEatRepository mustEatRepository;
@@ -26,7 +31,16 @@ public class MustEatService {
         return MustEatResponse.create(mustEat);
     }
 
+    @Transactional(readOnly = true)
+    public MustEatListResponse listMustEat() {
+        List<MustEatResponse> responses = mustEatRepository
+                .findAll()
+                .stream()
+                .map(MustEatResponse::create)
+                .toList();
 
+        return MustEatListResponse.create(responses);
+    }
 
 
 }
