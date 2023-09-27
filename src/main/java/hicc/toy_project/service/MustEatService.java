dcb1ab4d277f61804e76d35912ef25e3942617ee
leesh <qwqw8019@naver.com>
2023-09-28 04:37:19 +0900

    feat: 리뷰 쓰기 기능 구현 (#17)

--- src/main/java/hicc/toy_project/service/MustEatService.java
+++ src/main/java/hicc/toy_project/service/MustEatService.java
@@ -2,10 +2,18 @@ package hicc.toy_project.service;
 
 
 import hicc.toy_project.controller.dto.request.mustEat.MustEatCreateRequest;
+import hicc.toy_project.controller.dto.request.mustEat.ReviewCreateRequest;
 import hicc.toy_project.controller.dto.response.mustEat.MustEatListResponse;
 import hicc.toy_project.controller.dto.response.mustEat.MustEatResponse;
+import hicc.toy_project.controller.dto.response.mustEat.ReviewResponse;
+import hicc.toy_project.domain.member.Member;
 import hicc.toy_project.domain.mustEat.MustEat;
+import hicc.toy_project.domain.mustEat.Review;
+import hicc.toy_project.exception.CustomException;
+import hicc.toy_project.exception.ErrorCode;
+import hicc.toy_project.repository.MemberRepository;
 import hicc.toy_project.repository.MustEatRepository;
+import hicc.toy_project.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +26,8 @@ import java.util.List;
 public class MustEatService {
 
     private final MustEatRepository mustEatRepository;
+    private final MemberRepository memberRepository;
+    private final ReviewRepository reviewRepository;
 
     public MustEatResponse create(MustEatCreateRequest request) {
 
@@ -40,6 +50,27 @@ public class MustEatService {
                 .toList();
 
         return MustEatListResponse.create(responses);
+    }
+
+    public ReviewResponse writeReview(Long mustEatId,
+                                      String memberId,
+                                      ReviewCreateRequest request) {
+        MustEat mustEat = mustEatRepository.findById(mustEatId)
+                .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+
+        Member member = memberRepository.findByIdNumber(memberId)
+                .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+
+        Review review = Review.builder()
+                .mustEat(mustEat)
+                .member(member)
+                .content(request.getContent())
+                .score(request.getScore())
+                .build();
+
+        reviewRepository.save(review);
+
+        return ReviewResponse.create(review);
     }
 
 
