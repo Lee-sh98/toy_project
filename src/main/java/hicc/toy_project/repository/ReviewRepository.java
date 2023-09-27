e109ca7a4b18289ba659ff114d65e8070e2ac3ab
leesh <qwqw8019@naver.com>
2023-09-28 04:48:44 +0900

    feat: 맛집 id를 통한 리뷰 조회 쿼리메소드 추가 (#17)

--- src/main/java/hicc/toy_project/repository/ReviewRepository.java
+++ src/main/java/hicc/toy_project/repository/ReviewRepository.java
@@ -3,5 +3,9 @@ package hicc.toy_project.repository;
 import hicc.toy_project.domain.mustEat.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findAllByMustEatId(Long id);
 }
