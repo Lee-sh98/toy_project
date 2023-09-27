7a0d01f911ea31e2b43cb88577c8a50e21246c56
leesh <qwqw8019@naver.com>
2023-09-28 04:37:08 +0900

    feat: 리뷰 쓰기 컨트롤러 구현 (#17)

--- src/main/java/hicc/toy_project/controller/MustEatController.java
+++ src/main/java/hicc/toy_project/controller/MustEatController.java
@@ -1,8 +1,10 @@
 package hicc.toy_project.controller;
 
 import hicc.toy_project.controller.dto.request.mustEat.MustEatCreateRequest;
+import hicc.toy_project.controller.dto.request.mustEat.ReviewCreateRequest;
 import hicc.toy_project.controller.dto.response.mustEat.MustEatListResponse;
 import hicc.toy_project.controller.dto.response.mustEat.MustEatResponse;
+import hicc.toy_project.controller.dto.response.mustEat.ReviewResponse;
 import hicc.toy_project.service.MustEatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -24,5 +26,12 @@ public class MustEatController {
     public MustEatListResponse listMustEat() {
 
         return mustEatService.listMustEat();
+    }
+
+    @PostMapping("/{must-eat-id}")
+    public ReviewResponse createReview(@PathVariable("must-eat-id") Long mustEatId,
+                                       @RequestBody ReviewCreateRequest request) {
+        String memberId = "C011001";
+        return mustEatService.writeReview(mustEatId, memberId, request);
     }
 }
