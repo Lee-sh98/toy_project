87603f8b86bba56456fc39a5d37e057560dfda64
leesh <qwqw8019@naver.com>
2023-09-28 04:36:39 +0900

    feat: 리뷰 도메인 수정 (#17)
    작성자, 내용 추가
    빌더생성자 추가

--- src/main/java/hicc/toy_project/domain/mustEat/Review.java
+++ src/main/java/hicc/toy_project/domain/mustEat/Review.java
@@ -1,7 +1,9 @@
 package hicc.toy_project.domain.mustEat;
 
+import hicc.toy_project.domain.member.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,5 +20,23 @@ public class Review {
     @JoinColumn(name = "must_eat_id")
     private MustEat mustEat;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
     private Long score;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Builder
+    private Review(MustEat mustEat,
+                   Member member,
+                   Long score,
+                   String content) {
+        this.mustEat = mustEat;
+        this.member = member;
+        this.score = score;
+        this.content = content;
+    }
 }
