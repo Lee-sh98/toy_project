c1bf5285b0cd92eaf9929535718addef1903ab3f
leesh <qwqw8019@naver.com>
2023-10-06 21:06:56 +0900

    test: AdminPage 테스트 추가 (#10)
    - 회장 위임 기능
      - 정상 동작
    - 회원 등급 수정 기능
      - 정상 동작
      - 회장 본인 등급 수정 예외
      - 회장으로의 등급 수정 예외
      - 게스트로 등급 수정 예외
      - 같은 등급으로 수정 예외
    - 회원 제명 기능
      - 정상 동작
      - 회장 본인 제명 예외
    - 회원 가입 승인 기능
      - 정상 승인
      - 정상 반려
      - 지원자가 아닌 회원 승인 요청 예외

--- src/test/java/hicc/toy_project/adminpage/AdminPageTest.java
+++ src/test/java/hicc/toy_project/adminpage/AdminPageTest.java
@@ -2,6 +2,7 @@ package hicc.toy_project.adminpage;
 
 
 import hicc.toy_project.controller.dto.AdminPageRequest;
+import hicc.toy_project.controller.dto.ApproveRequest;
 import hicc.toy_project.domain.member.Member;
 import hicc.toy_project.domain.member.Role;
 import hicc.toy_project.exception.CustomException;
@@ -9,10 +10,7 @@ import hicc.toy_project.exception.ErrorCode;
 import hicc.toy_project.repository.MemberRepository;
 import hicc.toy_project.service.AdminPageService;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -35,7 +33,7 @@ public class AdminPageTest {
     MemberRepository memberRepository;
 
     String presidentId = "C011003";
-    String testTargetId = "C011004";
+    String testMemberId = "C011004";
 
     Member getMember(String id) {
         return memberRepository.findByIdNumber(id)
@@ -56,7 +54,7 @@ public class AdminPageTest {
                 .build();
 
         Member testTarget = Member.builder()
-                .id(testTargetId)
+                .id(testMemberId)
                 .nickName("testTarget")
                 .phoneNumber("011")
                 .role(Role.GENERAL)
@@ -71,20 +69,196 @@ public class AdminPageTest {
         transactionManager.rollback(status);
     }
 
-    @Test
-    @DisplayName("회장_위임_기능")
-    void presidentDelegateTest() {
-        Member currentPresident = getMember(presidentId);
-        Member nextPresident = getMember(testTargetId);
+    @Nested
+    @DisplayName("회장 위임 테스트")
+    class delegateTest {
+        @Test
+        @DisplayName("회장 권한 위임 후 기존 회장은 일반 회원이 된다.")
+        void success() {
+            Member currentPresident = getMember(presidentId);
+            Member nextPresident = getMember(testMemberId);
 
-        AdminPageRequest request = AdminPageRequest.builder()
-                .id(currentPresident.getIdNumber())
-                .targetId(nextPresident.getIdNumber())
-                .build();
+            AdminPageRequest request = AdminPageRequest.builder()
+                    .id(currentPresident.getIdNumber())
+                    .targetId(nextPresident.getIdNumber())
+                    .build();
 
-        adminPageService.delegate(request);
+            adminPageService.delegate(request);
 
-        Assertions.assertThat(currentPresident.getRole()).isEqualTo(Role.GENERAL);
-        Assertions.assertThat(nextPresident.getRole()).isEqualTo(Role.PRESIDENT);
+            Assertions.assertThat(currentPresident.getRole())
+                    .isEqualTo(Role.GENERAL);
+
+            Assertions.assertThat(nextPresident.getRole())
+                    .isEqualTo(Role.PRESIDENT);
+        }
+    }
+
+
+    @Nested
+    @DisplayName("회원 등급 수정 테스트")
+    class changeRoleTest {
+
+        @Test
+        @DisplayName("정상적으로 회원 등급을 임원으로 수정한다.")
+        void success() {
+            AdminPageRequest request = AdminPageRequest.builder()
+                    .id(presidentId)
+                    .targetId(testMemberId)
+                    .role(Role.EXECUTIVE)
+                    .build();
+
+            adminPageService.changeRole(request);
+
+            Assertions.assertThat(getMember(testMemberId).getRole())
+                    .isEqualTo(Role.EXECUTIVE);
+        }
+
+        @Test
+        @DisplayName("회장 본인의 등급을 수정시 발생하는 예외 테스트")
+        void selfUpdateException() {
+            AdminPageRequest request = AdminPageRequest.builder()
+                    .id(presidentId)
+                    .targetId(presidentId)  // 타겟 id를 회장으로 설정
+                    .role(Role.EXECUTIVE)
+                    .build();
+
+            Assertions.assertThatThrownBy(() -> adminPageService
+                            .changeRole(request))
+                    .isInstanceOf(CustomException.class);
+        }
+
+        @Test
+        @DisplayName("회장으로 직책을 변경시 발생하는 예외 테스트")
+        void updateIntoPresidentException() {
+            AdminPageRequest request = AdminPageRequest.builder()
+                    .id(presidentId)
+                    .targetId(testMemberId)
+                    .role(Role.PRESIDENT)
+                    .build();
+
+            Assertions.assertThatThrownBy(() -> adminPageService
+                            .changeRole(request))
+                    .isInstanceOf(CustomException.class);
+        }
+
+        @Test
+        @DisplayName("게스트로 등급을 변경시 발생하는 예외 테스트")
+        void updateIntoGuestException() {
+            AdminPageRequest request = AdminPageRequest.builder()
+                    .id(presidentId)
+                    .targetId(testMemberId)
+                    .role(Role.GENERAL)
+                    .build();
+
+            Assertions.assertThatThrownBy(() -> adminPageService
+                            .changeRole(request))
+                    .isInstanceOf(CustomException.class);
+        }
+
+        @Test
+        @DisplayName("같은 등급으로 변경시 발생하는 예외 테스트")
+        void updateIntoSameRoleException() {
+            AdminPageRequest request = AdminPageRequest.builder()
+                    .id(presidentId)
+                    .targetId(testMemberId)
+                    .role(Role.GENERAL)
+                    .build();
+
+            Assertions.assertThatThrownBy(() -> adminPageService
+                            .changeRole(request))
+                    .isInstanceOf(CustomException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("회원 제명 테스트")
+    class expelTest {
+
+        @Test
+        @DisplayName("성공적으로 일반 회원을 제명한다.")
+        void success() {
+            AdminPageRequest request = AdminPageRequest.builder()
+                    .id(presidentId)
+                    .targetId(testMemberId)
+                    .build();
+
+            Assertions.assertThat(adminPageService.expel(request))
+                    .isEqualTo(true);
+        }
+
+        @Test
+        @DisplayName("회장 본인을 제명할 때 발생하는 예외 테스트")
+        void selfExpelException() {
+            AdminPageRequest request = AdminPageRequest.builder()
+                    .id(presidentId)
+                    .targetId(presidentId)  // 타겟 id를 회장으로 설정
+                    .build();
+
+            Assertions.assertThatThrownBy(() -> adminPageService
+                            .expel(request))
+                    .isInstanceOf(CustomException.class);
+        }
+
+    }
+
+    @Nested
+    @DisplayName("회원 가입 승인 테스트")
+    class approveTest {
+        private final String testApplicantId = "C011005";
+
+        @BeforeEach
+        void beforeEach() {
+            Member applicant = Member.builder()
+                    .id(testApplicantId)
+                    .nickName("targetApplicant")
+                    .phoneNumber("012")
+                    .role(Role.GUEST)
+                    .build();
+
+            memberRepository.save(applicant);
+        }
+
+
+        @Test
+        @DisplayName("지원자를 승인한다.")
+        void successApproval() {
+            AdminPageRequest request = AdminPageRequest.builder()
+                    .id(presidentId)
+                    .targetId(testApplicantId)
+                    .approveRequest(ApproveRequest.APPROVE)
+                    .build();
+
+            adminPageService.approve(request);
+
+            Assertions.assertThat(getMember(testApplicantId).getRole())
+                    .isEqualTo(Role.GENERAL);
+
+        }
+
+        @Test
+        @DisplayName("지원자를 반려한다.")
+        void successRejection() {
+            AdminPageRequest request = AdminPageRequest.builder()
+                    .id(presidentId)
+                    .targetId(testApplicantId)
+                    .approveRequest(ApproveRequest.REJECT)
+                    .build();
+
+            Assertions.assertThat(adminPageService.approve(request))
+                    .isEqualTo(true);
+        }
+
+        @Test
+        @DisplayName("타겟 id가 게스트가 아닐 때 발생하는 예외 테스트")
+        void approveNotGuestException() {
+            AdminPageRequest request = AdminPageRequest.builder()
+                    .id(presidentId)
+                    .targetId(testMemberId) // 지원자가 아닌 일반 회원 id를 설정
+                    .build();
+
+            Assertions.assertThatThrownBy(() -> adminPageService
+                            .approve(request))
+                    .isInstanceOf(CustomException.class);
+        }
     }
 }
